9bb4c595c7abcdeaa568cc15c28c1b1c293c4319
Andreas Fichtner <andreas.fichtner@gmail.com>
2016-08-10 15:36:29 +0200

    fixed jetty version, added login page

--- src/main/java/de/retterdesapok/jettydooropener/OpenDoorServlet.java
+++ src/main/java/de/retterdesapok/jettydooropener/OpenDoorServlet.java
@@ -9,44 +9,40 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class OpenDoorServlet extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	public static String PARAM_USERNAME = "username";
 	public static String PARAM_PASSWORD = "password";
-	
-	@Override 
-	  public void doGet(HttpServletRequest request, HttpServletResponse response) 
-	      throws IOException
-	  {
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
 		String username = request.getParameter(PARAM_USERNAME);
 		String password = request.getParameter(PARAM_PASSWORD);
-		
-		if(username == null || password == null) {
-			response.getOutputStream().println("Invalid arguments sent. Expected username and password");
+
+		if (username == null || username.length() == 0 || password == null || password.length() == 0) {
+			response.getOutputStream().println("Invalid arguments sent. Expected username and password.");
 			response.setStatus(400);
 			return;
 		}
 
 		response.getOutputStream().println("Authenticating...");
 		String passwordmd5 = null;
-		
-		if(password != null && password.length() > 0) {
-			try {
-				passwordmd5 = Utilities.getMD5(password);
-			} catch (NoSuchAlgorithmException e) {
-				response.getOutputStream().println(e.getLocalizedMessage());
-				response.getOutputStream().println(e.getStackTrace().toString());
-				response.setStatus(500);
-				return;
-			}
+
+		try {
+			passwordmd5 = Utilities.getMD5(password);
+		} catch (NoSuchAlgorithmException e) {
+			response.getOutputStream().println(e.getLocalizedMessage());
+			response.getOutputStream().println(e.getStackTrace().toString());
+			response.setStatus(500);
+			return;
 		}
-		
-		if(passwordmd5 != null) {
+
+		if (passwordmd5 != null) {
 			try {
 				User user = DatabaseHelper.loginUserWithNameAndPasswordHash(username, passwordmd5);
-				if(user == null || user.getFailedLoginCount() > 3) {
+				if (user == null || user.getFailedLoginCount() > 3) {
 					response.getOutputStream().println("Wrong username or password.");
 					response.setStatus(403);
 					return;
@@ -54,7 +50,7 @@ public class OpenDoorServlet extends HttpServlet {
 					response.getOutputStream().println("No logins left...");
 					response.setStatus(403);
 					return;
-				} else if(user.getFailedLoginCount() <= 3 && user.getRemainingLogins() > 0) {
+				} else if (user.getFailedLoginCount() <= 3 && user.getRemainingLogins() > 0) {
 					response.getOutputStream().println("Activating relay...");
 					response.setStatus(200);
 				}
@@ -65,7 +61,6 @@ public class OpenDoorServlet extends HttpServlet {
 				return;
 			}
 		}
-	  }
-	
-	
+	}
+
 }
